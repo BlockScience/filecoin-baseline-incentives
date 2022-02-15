698740fa592900e817690301bc3b733bb3b7f94a
Danillo Lessa Bernardineli <danilo.lessa@gmail.com>
2022-02-15 09:07:14 -0500

    folder struct

---
+++ baseline_model/__main__.py
@@ -0,0 +1,16 @@
+from baseline_model import default_run_args
+from cadCAD_tools.execution import easy_run
+from datetime import datetime
+import click
+import os
+
+
+@click.command()
+def main() -> None:
+    df = easy_run(*default_run_args)
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    df.to_pickle(f"data/simulation_output/{timestamp}.pkl.gz", compression="gzip")
+
+
+if __name__ == "__main__":
+    main()
