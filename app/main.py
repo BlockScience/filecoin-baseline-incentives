25572cfdf0612acffd199da3abbcb1dc923f18ac
Danillo Lessa Bernardineli <danilo.lessa@gmail.com>
2022-02-24 12:42:29 -0300

    cosmetic changes

--- app/main.py
+++ app/main.py
@@ -26,18 +26,12 @@ st.set_page_config(page_title="Filecoin Baseline Minting Educational Calculator"
                        __file__), "assets", "icon.png"),
                    layout="wide")
 
-_, image_container, _ = st.columns([1, 4, 1])
-
-with image_container:
-    st.image(os.path.join(os.path.dirname(__file__),
-             "assets", "logo.png"), width=800)
-
 st.markdown("# Filecoin Baseline Minting Educational Calculator")
 
 st.markdown("""
 This app allows you to **interactively understand how Baseline Minting works** in terms of mining incentives and what happens **when the Baseline Function is crossed up or down**.
 
-You have full control over how the raw-bytes Network Power looks like on the future! That's the `user`, and by tweaking the `When?` and `How fast?` fields for each stage, you can see **how it behaves and compares to an `optimistic` scenario, and against itself when Baseline Minting is turned off**.
+You have full control over how the raw-bytes Network Power looks like on the future! That's the `user`, and by tweaking the `How long? (in years since last change)` and `rb-NP growth (as a fraction of the baseline growth)` fields for each stage, you can see **how it behaves and compares to an `optimistic` scenario, and against itself when Baseline Minting is turned off**.
 """)
 
 with st.expander("See description"):
@@ -57,7 +51,14 @@ download_container = st.container()
 
 # Define sidebar
 
-st.sidebar.markdown("# `user` scenario Parameters")
+
+_, image_container, _ = st.sidebar.columns([1, 2, 1])
+
+with image_container:
+    st.image(os.path.join(os.path.dirname(__file__),
+             "assets", "icon.png"))
+
+st.sidebar.markdown("# Parameters for the `user` scenario")
 
 defaults = C["network_power"]["pessimistic"]
 
@@ -67,57 +68,52 @@ The trajectory is described as being:
 1. First, the rb-NP is set as an fixed initial value.
 2. For an given duration, the rb-NP is going grow as an fraction of the Baseline Function growth. Those are the parameters for the (Cross BaseFunc from Above) stage.
 3. Repeat 2. for the remaining phases.
-
-For each change, we ask:
-
-- When? (Years Since Previous Change)
-- How fast? (as an fraction of the Baseline Function Growth)
 """
 )
 
-st.sidebar.markdown("### 1️⃣ Cross BaseFunc From Above")
+st.sidebar.markdown("### 1️⃣ Cross Baseline Function From Above")
 
 fall_after_beginning = (
     st.sidebar.slider(
-        "When?", 0.0, 8.0, defaults["fall_after_beginning"] / C["days_per_year"], 0.25, key="fall")
+        "How long? (in years since last change)", 0.0, 8.0, defaults["fall_after_beginning"] / C["days_per_year"], 0.25, key="fall")
     * C["days_per_year"]
 )
 
 growth_fall = st.sidebar.slider(
-    "How fast?", -0.2, 1.2, defaults["growth_fall"], 0.1, key="fall")
+    "rb-NP growth (as a fraction of the baseline growth)", -0.2, 1.2, defaults["growth_fall"], 0.1, key="fall")
 
-st.sidebar.markdown("### 2️⃣ Stabilize Below BaseFunc")
+st.sidebar.markdown("### 2️⃣ Stabilize Below Baseline Function")
 
 stable_after_fall = (
     st.sidebar.slider(
-        "When?", 0.0, 8.0, defaults["stable_after_fall"] / C["days_per_year"], 0.25, key="stable")
+        "How long? (in years since last change)", 0.0, 8.0, defaults["stable_after_fall"] / C["days_per_year"], 0.25, key="stable")
     * C["days_per_year"]
 )
 
 growth_stable = st.sidebar.slider(
-    "How fast?", -0.2, 1.2, defaults["growth_stable"], 0.1, key="stable")
+    "rb-NP growth (as a fraction of the baseline growth)", -0.2, 1.2, defaults["growth_stable"], 0.1, key="stable")
 
-st.sidebar.markdown("### 3️⃣ Recross BaseFunc from Below")
+st.sidebar.markdown("### 3️⃣ Recross Baseline Function from Below")
 
 take_off_after_stable = (
     st.sidebar.slider(
-        "When?", 0.0, 8.0, defaults["take_off_after_stable"] / C["days_per_year"], 0.25, key="take_off")
+        "How long? (in years since last change)", 0.0, 8.0, defaults["take_off_after_stable"] / C["days_per_year"], 0.25, key="take_off")
     * C["days_per_year"]
 )
 
 growth_take_off = st.sidebar.slider(
-    "How fast?", -0.2, 8.0, defaults["growth_take_off"], 0.1, key="take_off")
+    "rb-NP growth (as a fraction of the baseline growth)", -0.2, 8.0, defaults["growth_take_off"], 0.1, key="take_off")
 
-st.sidebar.markdown("### 4️⃣ Stabilize Above BaseFunc")
+st.sidebar.markdown("### 4️⃣ Stabilize Above Baseline Function")
 
 steady_after_take_off = (
     st.sidebar.slider(
-        "When?", 0.0, 8.0, defaults["steady_after_take_off"] / C["days_per_year"], 0.25, key="steady")
+        "How long? (in years since last change)", 0.0, 8.0, defaults["steady_after_take_off"] / C["days_per_year"], 0.25, key="steady")
     * C["days_per_year"]
 )
 
 growth_steady = st.sidebar.slider(
-    "How fast?", -0.2, 1.2, defaults["growth_steady"], 0.1, key="steady")
+    "rb-NP growth (as a fraction of the baseline growth)", -0.2, 1.2, defaults["growth_steady"], 0.1, key="steady")
 
 st.sidebar.markdown("## Compare Against")
 
